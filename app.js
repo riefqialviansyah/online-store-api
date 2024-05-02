6fae7630c6787cc337a4c2f2e5597963752a4fa8
riefqialviansyah <riefqi.alviansyah@gmail.com>
2024-05-02 08:10:40 +0800

    setup for deploy api in aws

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const express = require("express");
 const app = express();
+
+// user corse
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
